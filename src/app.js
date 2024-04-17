83b32fc6d7590f99601204f0294b9be87dddcfde
Abhishek Kumar <absk08072002@gmail.com>
2024-04-17 20:30:50 +0530

    utils code discussion

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
+const app = express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))  // for special characters like 
+           // space - %20 etc.
+app.use(express.static("public"))  //for storing files and folders
+app.use(cookieParser()) // for access and set the cookies from my server (crud operations)
+
+export { app }
+
+
+// middleware - if we are requesting, a response will be given by the server. so server check if you are capable of taking the response. (eg: if you are logged in or not) this checking process is known as middleware.
